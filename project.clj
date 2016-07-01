2840c356d6a2d064b650557c8642a7d99f56a6f9
Tom Faulhaber <git_com@infolace.com>
2016-07-01 12:44:20 -0700

    Add stuff to project.clj to support lein release

--- project.clj
+++ project.clj
@@ -1,3 +1,6 @@
-(defproject autodoc/lein-autodoc "1.1.0"
+(defproject autodoc/lein-autodoc "1.1.1-SNAPSHOT"
   :description "A leiningen plug-in for the autodoc documentation generator"
-  :dependencies [])
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies []
+  :deploy-repositories [["releases" :clojars]])
